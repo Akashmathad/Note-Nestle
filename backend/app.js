485967048a1e1c995d07415322b32671dccd06aa
Akash Mathad <akashmathad002@gmail.com>
2024-01-13 15:53:56 +0530

    initial setup

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,36 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const compression = require('compression');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(helmet());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(compression());
+
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () =>
-  console.log(`Note-Nestle app listening on port ${port}!`)
-);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+module.exports = app;
