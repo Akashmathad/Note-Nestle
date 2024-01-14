09633aafc3ddcfc36c4625ba4c883d760056563a
Akash Mathad <akashmathad002@gmail.com>
2024-01-14 14:37:24 +0530

    studentModel

---
+++ backend/controllers/authController.js
@@ -0,0 +1,4 @@
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const APIFeatures = require('./../utils/apiFeatures');
+const Student = require('./../models/studentModel');
---
+++ backend/models/studentModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  collegeId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  teacherAccess: {
+    type: Boolean,
+    default: false,
+  },
+  adminAccess: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+module.exports = Student;
