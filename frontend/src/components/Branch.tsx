fd944c62cbd517ac84c7dc4b3529276d4f0517b7
Akash Mathad <akashmathad002@gmail.com>
2024-02-04 22:12:02 +0530

    home page

--- frontend/src/components/Branch.tsx
+++ frontend/src/components/Branch.tsx
@@ -3,18 +3,18 @@ import Link from 'next/link';
 
 const Branch = ({ branch }) => {
   return (
-    <div className=" border-borderN border-[1px] rounded-[0.5rem] p-[2rem]  text-start justify-start items-start">
-      <div className="mb-[1.5rem]  text-slate-400">{branch.icon}</div>
+    <Link
+      href={`/${branch.branch}`}
+      className=" border-borderN border-[1px] rounded-[0.5rem] p-[2rem]  text-start justify-start items-start"
+    >
+      <div className="mb-[1.5rem] text-title">{branch.icon}</div>
       <div className="mb-[1.5rem]">
-        <Link
-          className="text-[1.5rem] lg:text-[2rem] font-fontPrimary text-title"
-          href={`/${branch.branch}`}
-        >
+        <p className="text-[1.5rem] lg:text-[2rem] font-fontPrimary text-title leading-[1.2]">
           {branch.name}
-        </Link>
+        </p>
       </div>
       <div className="text-base text-para">{branch.description}</div>
-    </div>
+    </Link>
   );
 };
 
